24ebcc2024315b2294d7f705aae2653300454644
Vavassori Giacomo <giacomo.vavassori@scania.com>
2020-12-13 22:20:17 +0100

    final solution working

--- day5/solver_part1.py
+++ day5/solver_part1.py
@@ -7,54 +7,53 @@ def locator(string, type):
     row_start = 0
     for e in string:
         midpoint = int((row_end-row_start)/2)
+        midpoint = midpoint + row_start
         if e == 'B' or e == 'R':
             row_start = midpoint
-            print('new row_start:', midpoint)
+            #print('new row_start:', midpoint)
         else:
-            row_end = row_start -1 + midpoint
-            print('new row_end:', midpoint)
+            row_end =  midpoint
+            #print('new row_end:', midpoint)
     return row_end
 
 fhandler = open('input.txt')
 seat_ID_list = []
 seat_ID_list_t = []
 
-# for line in fhandler:
-#     seat = line.rstrip('\n')
-#     row = seat[:7]
-#     col = seat[7:]
-#     rnum = locator(row, 'row')
-#     cnum = locator(col,'col')
-#     print('row nr:', rnum)
-#     print('col nr:', cnum)
-#     seat_ID = rnum*8 + cnum
-#     print('ID:', seat_ID)
-#     seat_ID_list.append(seat_ID)
-
-# print('maximum seat ID {}'.format(max(seat_ID_list)))
-
-test1 = 'BFFFBBFRRR'
-test2 = 'FFFBBBFRRR'
-test3 = 'BBFFBBFRLL'
-
-test = [test1, test2, test3]
-
-print('========TEST AREA========')
-for line in test:
+for line in fhandler:
     seat = line.rstrip('\n')
     row = seat[:7]
-    print('row ID:', row)
     col = seat[7:]
-    print('col ID:', col)
     rnum = locator(row, 'row')
     cnum = locator(col,'col')
     print('row nr:', rnum)
     print('col nr:', cnum)
-    seat_IDt = rnum*8 + cnum
-    print('ID:', seat_IDt)
-    seat_ID_list_t.append(seat_IDt)
-    input('enter .-.....')
-    
+    seat_ID = rnum*8 + cnum
+    print('ID:', seat_ID)
+    seat_ID_list.append(seat_ID)
 
-print('maximum TEST seat ID {}'.format(max(seat_ID_list_t)))
+print('maximum seat ID {}'.format(max(seat_ID_list)))
+
+# test1 = 'BFFFBBFRRR'
+# test2 = 'FFFBBBFRRR'
+# test3 = 'BBFFBBFRLL'
+
+# test = [test1, test2, test3]
+
+# print('========TEST AREA========')
+# for line in test:
+#     seat = line.rstrip('\n')
+#     row = seat[:7]
+#     print('row ID:', row)
+#     col = seat[7:]
+#     print('col ID:', col)
+#     rnum = locator(row, 'row')
+#     cnum = locator(col,'col')
+#     print('row nr:', rnum)
+#     print('col nr:', cnum)
+#     seat_IDt = rnum*8 + cnum
+#     print('ID:', seat_IDt)
+#     seat_ID_list_t.append(seat_IDt)
+#     #input('enter .-.....')
+# print('maximum TEST seat ID {}'.format(max(seat_ID_list_t)))
 
