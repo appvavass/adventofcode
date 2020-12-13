962626c28a9aaf3fad507de121f5d1edca1ef8d6
Vavassori Giacomo <giacomo.vavassori@scania.com>
2020-12-13 20:54:21 +0100

    first part done

--- day5/solver_part1.py
+++ day5/solver_part1.py
@@ -2,4 +2,26 @@ seat = 'BBFFBFFRRR'
 
 row = seat[:7]
 col = seat[7:]
+##Converting input into mathematical good stuff
 
+row_lst = []
+col_lst = []
+for e in row:
+    if e == 'B':
+        row_lst.append(1)
+    else:
+        row_lst.append(-1)
+
+for e in col:
+    if e == 'L':
+        col_lst.append(1)
+    else:
+        col_lst.append(-1)
+
+print(row_lst)
+print(col_lst)
+
+def row_locator(lst):
+    start_row = 127
+    for e in lst:
+        new_row = start_row + start_row/2  
