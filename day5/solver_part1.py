2f81f1029c61ab562972d65bf38ae2d26009ae3c
Vavassori Giacomo <giacomo.vavassori@scania.com>
2020-12-13 21:31:23 +0100

    wip

--- day5/solver_part1.py
+++ day5/solver_part1.py
@@ -1,27 +1,29 @@
-seat = 'BBFFBFFRRR'
-
-row = seat[:7]
-col = seat[7:]
-##Converting input into mathematical good stuff
-
-row_lst = []
-col_lst = []
-for e in row:
-    if e == 'B':
-        row_lst.append(1)
+def locator(string, type):
+    if type == 'row':
+        row_end = 127
     else:
-        row_lst.append(-1)
+        row_end = 7
+    row_start = 0
+    for e in string:
+        midpoint = int(row_end/2)
+        if e == 'B':
+            row_start = midpoint
+        else:
+            row_end = midpoint
+    return row_end
 
-for e in col:
-    if e == 'L':
-        col_lst.append(1)
-    else:
-        col_lst.append(-1)
+fhandler = open('input.txt')
+seat_ID_list = []
 
-print(row_lst)
-print(col_lst)
+for line in fhandler:
+    seat = line.rstrip('\n')
+    row = seat[:7]
+    col = seat[7:]
+    rnum = locator(row, 'row')
+    cnum = locator(col,'col')
+    print('row nr:', rnum)
+    print('col nr:', cnum)
+    seat_ID = rnum*8 + cnum
+    seat_ID_list.append(seat_ID)
 
-def row_locator(lst):
-    start_row = 127
-    for e in lst:
-        new_row = start_row + start_row/2  
+print('maximum seat ID {}'.format(max(seat_ID_list)))
