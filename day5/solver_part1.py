4a9483a35f67f5b4f01b15360e9e3baa8ea94dd9
Vavassori Giacomo <giacomo.vavassori@scania.com>
2020-12-13 21:57:27 +0100

    none

--- day5/solver_part1.py
+++ day5/solver_part1.py
@@ -1,29 +1,60 @@
 def locator(string, type):
+    print('checking for', type)
     if type == 'row':
         row_end = 127
     else:
         row_end = 7
     row_start = 0
     for e in string:
-        midpoint = int(row_end/2)
-        if e == 'B':
+        midpoint = int((row_end-row_start)/2)
+        if e == 'B' or e == 'R':
             row_start = midpoint
+            print('new row_start:', midpoint)
         else:
-            row_end = midpoint
+            row_end = row_start -1 + midpoint
+            print('new row_end:', midpoint)
     return row_end
 
 fhandler = open('input.txt')
 seat_ID_list = []
+seat_ID_list_t = []
 
-for line in fhandler:
+# for line in fhandler:
+#     seat = line.rstrip('\n')
+#     row = seat[:7]
+#     col = seat[7:]
+#     rnum = locator(row, 'row')
+#     cnum = locator(col,'col')
+#     print('row nr:', rnum)
+#     print('col nr:', cnum)
+#     seat_ID = rnum*8 + cnum
+#     print('ID:', seat_ID)
+#     seat_ID_list.append(seat_ID)
+
+# print('maximum seat ID {}'.format(max(seat_ID_list)))
+
+test1 = 'BFFFBBFRRR'
+test2 = 'FFFBBBFRRR'
+test3 = 'BBFFBBFRLL'
+
+test = [test1, test2, test3]
+
+print('========TEST AREA========')
+for line in test:
     seat = line.rstrip('\n')
     row = seat[:7]
+    print('row ID:', row)
     col = seat[7:]
+    print('col ID:', col)
     rnum = locator(row, 'row')
     cnum = locator(col,'col')
     print('row nr:', rnum)
     print('col nr:', cnum)
-    seat_ID = rnum*8 + cnum
-    seat_ID_list.append(seat_ID)
+    seat_IDt = rnum*8 + cnum
+    print('ID:', seat_IDt)
+    seat_ID_list_t.append(seat_IDt)
+    input('enter .-.....')
+    
 
-print('maximum seat ID {}'.format(max(seat_ID_list)))
+print('maximum TEST seat ID {}'.format(max(seat_ID_list_t)))
+
