3ad0f55f57da92353102b5ba8682f7fdce487957
Vavassori Giacomo <giacomo.vavassori@scania.com>
2020-12-14 21:29:24 +0100

    aa

--- day7/solver_part1.py
+++ day7/solver_part1.py
@@ -1,19 +1,22 @@
 import re
 
 fhandle = open('input.txt')
-lst_bag = []
+lst_bag = {}
 
 for row in fhandle:
     row = row.split('contain')
     left_pattern = '(^.*) bag*'
     bag = row[0]
     bag_color = re.findall(left_pattern, bag)
-    
+    bag_color = bag_color[0]
     row[1] = row[1].rstrip('.\n')
     rule = row[1]
+    rule = rule.split(',')
+    lst_bag[bag_color] = rule
+    
+print(lst_bag)
 
-
-    lst_bag.append(bag_color[0])
+    
     
 
 
