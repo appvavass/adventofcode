da4f9b3d7d1b2a62f213ab7c3453a1ede3d2488a
Vavassori Giacomo <giacomo.vavassori@scania.com>
2020-12-14 11:33:05 +0100

    part1 solution

--- day6/loser_part1.py
+++ day6/loser_part1.py
@@ -17,8 +17,22 @@ for el in big_ans:
         group_ans.append(ans)
         ans = ''
 
-print(group_ans[:4])
+total_yes = 0
 
+#print(group_ans)
+
+for e in group_ans:
+    comparator = []
+    for letter in e:
+        #print('checking letter', letter)
+        if letter not in comparator:
+            comparator.append(letter)
+        else: continue
+    print(comparator)
+    score = len(comparator)
+    total_yes = total_yes + score
+
+print('sum of yes ansers is {}'.format(total_yes))
 
 
 
