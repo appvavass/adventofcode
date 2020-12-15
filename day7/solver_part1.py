53d55f46d71fcea876488544e61bb890e104d51f
g v <appvavass@gmail.com>
2020-12-15 18:56:16 +0100

    formatting

--- day7/solver_part1.py
+++ day7/solver_part1.py
@@ -4,8 +4,11 @@ fhandle = open('input.txt')
 lst_bag = {}
 lst_rules = []
 
+########################################################
+#divide in bag - rules and create a dictionary {bag color: bag rules}
+########################################################
+
 for row in fhandle:
-    #divide in bag - rules and create a dictionary {bag color: bag rules}
     row = row.split('contain')
     left_pattern = '(^.*) bag.*'
     bag = row[0]
@@ -21,7 +24,10 @@ for row in fhandle:
         lst_bag[bag_color] = ruleset
 #print('length of rules:',len(lst_bag))
 
+########################################################
 #check how many bags can contain directly the shiny gold
+########################################################
+
 iterate = True
 validbags =  ['.*shiny gold.*']
 it = 1
@@ -29,8 +35,6 @@ bag_to_skip = []
 list_len = {}
 while iterate is True: 
     iterator_key = validbags
-    #print('check1:',check1)
-    #print('valid bags:', validbags)
     validbags_new = []
     for key in lst_bag:
         check = lst_bag.get(key)
@@ -43,19 +47,14 @@ while iterate is True:
                         validbags_new.append(to_append)
                         #print('iteration {}, found this colors {}'.format(it, validbags_new))
     bag_to_skip  = bag_to_skip + validbags
-
-    #print('bags to skip:', bag_to_skip)
-    #print('new valid bags:', validbags_new)
     validbags = validbags_new
     if iterator_key == validbags:
         iterate = False
-    #print('2nd check',check1)
     list_len[it] = len(validbags_new)
     it = it + 1
-    #print(list_len)
     #input('enter................')
 
-bag_to_skip.sort()
+
 #print(bag_to_skip)
 print(list_len)   
 print('puzzle answer is',len(bag_to_skip)-1)
