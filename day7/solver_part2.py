603fe06ce94324ce313f2352a4aac3d0d5705a88
Vavassori Giacomo <giacomo.vavassori@scania.com>
2020-12-17 17:20:36 +0100

    partially working solver

--- day7/solver_part2.py
+++ day7/solver_part2.py
@@ -18,44 +18,47 @@ for row in fhandle:
     rule = row[1]
     right_pattern = '([0-9]+)\s([a-z]+\s[a-z]+)' ## two words
     ruleset  = re.findall(right_pattern, rule)
-    #rule = rule.split(',')
+    if len(ruleset) == 0:
+        ruleset = [(0, 'other bags')]
     #lst_bag[bag_color] = rule
-    if len(ruleset) > 0:
-        lst_bag[bag_color] = ruleset
-#print(lst_bag)
+    lst_bag[bag_color] = ruleset
+        
+#print('LIST_BAG',lst_bag)
 
-def bagcounter(dictionary,colors):
+def bagcounter(dictionary,color,constant):
 ## This function goes trhough the dictionary and look for the input colors, 
 # return what those colors must contain 
     new_colors = []
     return_dict = {}
-    for color in colors:
-        value = dictionary.get(color)
-        if value is None: return
-        else:
-            for e in value:
-                n_bag = e[0]
-                color_bags = e[1]
-                if color_bags not in new_colors:
-                    new_colors.append(color_bags)
-                    return_dict[color_bags]  = int(n_bag) 
+    value = dictionary.get(color)
+    if value == [(0, 'other bags')]:
+        return 0
+    else:
+        for e in value:
+            n_bag = e[0]
+            color_bags = e[1]
+            if color_bags not in new_colors:
+                new_colors.append(color_bags)
+                return_dict[color_bags]  = int(n_bag)*constant 
         #print('function out:', return_dict)
         return return_dict
 
-init_clue = ['shiny gold']
-tier = 0
-solution = []
+new_clue = [('shiny gold',1)]
+bags_sum = []
 
-while True:
-    answer = bagcounter(lst_bag,init_clue)
-    solution.append((tier, answer))
-    tier +=1
-    init_clue = []
-    #print('solution', solution)
-    #print('answer:', answer)
-    if answer is not None:
-        for k in answer:
-            init_clue.append(k)
-    else: break
+for i in range(1,100):
+    for col,const in new_clue:
+        bags = bagcounter(lst_bag, col, const)
+        if bags == 0: 
+            continue
+        new_clue = []
+        for k,v in bags.items():
+            clue = (k,v)
+            new_clue.append(clue)
+            bags_sum.append(v)
+    #print(bags_sum)
+    #print('new_clue', new_clue)
 
-print(solution)
+print(sum(bags_sum))
+
+
