2897e2ae274ce303a48a63f9dbd8d335ec51286e
Vavassori Giacomo <giacomo.vavassori@scania.com>
2020-12-19 10:01:06 +0100

    part 1 solved

--- day3/solver.py
+++ day3/solver.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-init =  open('input.txt')
+init =  open('input_test.txt')
 countrow = 0
 countcol = 0
 
--- day8/solver.py
+++ day8/solver.py
@@ -23,10 +23,14 @@ accumulator = 0
 it = 0
 
 while True:
+    print('accumulator before next step is {}'.format(accumulator))
     operation, val_to_add, ex_count = instructions.iloc[startline]
+    ex_count += 1
+    instructions['ex_counter'][startline]  = ex_count
     new_acc, newline = executor(startline, operation,accumulator,val_to_add)
     it = it + 1
     startline = newline
     accumulator = new_acc
-    if it == 2: break
+    if ex_count == 3: break
+
 
