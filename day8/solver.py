9a077b1755482dcbc797e120909b4c0c650954b0
Vavassori Giacomo <giacomo.vavassori@scania.com>
2020-12-19 11:28:06 +0100

    solution 1

--- day8/solver.py
+++ day8/solver.py
@@ -15,22 +15,23 @@ instructions = pd.read_csv('input.txt', sep=' ', header = None, names=('operatio
 
 instructions['ex_counter'] = 1
 
-print(instructions.iloc[0])
-
-
+#print('ciaooooo', instructions.loc[0,'ex_counter'])
 startline = 0
 accumulator = 0
 it = 0
 
 while True:
-    print('accumulator before next step is {}'.format(accumulator))
+    old_acc = accumulator
     operation, val_to_add, ex_count = instructions.iloc[startline]
     ex_count += 1
-    instructions['ex_counter'][startline]  = ex_count
+    #instructions['ex_counter'][startline]  = ex_count
+    instructions.loc[startline,'ex_counter'] = ex_count
     new_acc, newline = executor(startline, operation,accumulator,val_to_add)
     it = it + 1
     startline = newline
     accumulator = new_acc
-    if ex_count == 3: break
+    if ex_count == 3: 
+        print('Answer for part 1 is:',old_acc)
+        break
 
 
